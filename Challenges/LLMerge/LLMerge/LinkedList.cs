034ff8ba791535bb8e21481ae45430bcc3a19edd
Karina Chen <karina6188@gmail.com>
2019-10-24 23:59:15 -0700

    add codes

---
+++ Challenges/LLMerge/LLMerge/LinkedList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LLMerge
+{
+    public class LinkedList
+    {
+        public Node Head { get; set; }
+
+        public LinkedList()
+        {
+            Head = null;
+        }
+    }
+}
