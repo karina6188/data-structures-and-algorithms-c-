068c39bd52e9360b2a43d72b35cb51d1cec7ae0d
Karina Chen <karina6188@gmail.com>
2019-11-18 23:49:14 -0800

    complete unit testing

--- Challenges/InsertionSort/InsertionSortTests/UnitTest1.cs
+++ Challenges/InsertionSort/InsertionSortTests/UnitTest1.cs
@@ -28,11 +28,21 @@ namespace InsertionSortTests
         }
 
         [Fact]
-        public void CanSortUniqueArray()
+        public void CanSortReservedArray()
         {
-            int[] array = new int[] { 41, 29, 15, 7, 13, 24, 33, 36 };
+            int[] array = new int[] { 65, 60, 55, 50, 45, 40, 35, 30 };
             InsertionSort(array);
-            int[] test = new int[] { 7, 13, 15, 24, 29, 33, 36, 41 };
+            int[] test = new int[] { 30, 35, 40, 45, 50, 55, 60, 65 };
+
+            Assert.Equal(test, array);
+        }
+
+        [Fact]
+        public void CanSortNearlySorted()
+        {
+            int[] array = new int[] { 2, 4, 6, 8, 10, 12, 14, 16, -10, 0 };
+            InsertionSort(array);
+            int[] test = new int[] { -10, 0, 2, 4, 6, 8, 10, 12, 14, 16 };
 
             Assert.Equal(test, array);
         }
