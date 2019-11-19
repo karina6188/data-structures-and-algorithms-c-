20d839c996b31d7beb67c89b6b5460e1ac6e5b96
Karina Chen <karina6188@gmail.com>
2019-11-18 23:47:06 -0800

    add test 1

--- Challenges/InsertionSort/InsertionSortTests/UnitTest1.cs
+++ Challenges/InsertionSort/InsertionSortTests/UnitTest1.cs
@@ -10,9 +10,31 @@ namespace InsertionSortTests
         [Fact]
         public void CanSortUniqueArray()
         {
-            int[] array1 = new int[] { 41, 29, 15, 7, 13, 24, 33, 36 };
-            InsertionSort(array1);
+            int[] array = new int[] { 41, 29, 15, 7, 13, 24, 33, 36 };
+            InsertionSort(array);
+            int[] test = new int[] { 7, 13, 15, 24, 29, 33, 36, 41 };
 
+            Assert.Equal(test, array);
+        }
+
+        [Fact]
+        public void CanSortSameValues()
+        {
+            int[] array = new int[] { 15, 11, 18, 6, 11, 3, 13, 3, 5 };
+            InsertionSort(array);
+            int[] test = new int[] { 3, 3, 5, 6, 11, 11, 13, 15, 18 };
+
+            Assert.Equal(test, array);
+        }
+
+        [Fact]
+        public void CanSortUniqueArray()
+        {
+            int[] array = new int[] { 41, 29, 15, 7, 13, 24, 33, 36 };
+            InsertionSort(array);
+            int[] test = new int[] { 7, 13, 15, 24, 29, 33, 36, 41 };
+
+            Assert.Equal(test, array);
         }
     }
 }
