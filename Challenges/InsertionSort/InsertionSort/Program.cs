6f213b7a6a877cc8292e8ce7f9a07ec8bc795e26
Karina Chen <karina6188@gmail.com>
2019-11-18 23:19:28 -0800

    complete InsertionSort method

--- Challenges/InsertionSort/InsertionSort/Program.cs
+++ Challenges/InsertionSort/InsertionSort/Program.cs
@@ -6,10 +6,16 @@ namespace InsertionSort
     {
         static void Main(string[] args)
         {
-            
+            int[] array1 = new int[] { 8, 4, 23, 42, 16, 15 };
+            InsertionSort(array1);
+            Console.WriteLine("Sort array [8, 4, 23, 42, 16, 15] using insertion sort:");
+            for (int i = 0; i < array1.Length; i++)
+            {
+                Console.WriteLine();
+            }
         }
 
-        static void InsertSort(int[] arr)
+        static void InsertionSort(int[] arr)
         {
             for (int i = 1; i < arr.Length; i++)
             {
@@ -19,7 +25,8 @@ namespace InsertionSort
                 while (j >= 0 && temp < arr[j])
                 {
                     arr[j + 1] = arr[j];
-
+                    j = j - 1;
+                    arr[j + 1] = temp;
                 }
             }
         }
