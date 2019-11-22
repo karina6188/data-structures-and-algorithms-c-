b9353ed0a3d1a088bba4704df36e0fb0afe68051
Karina Chen <karina6188@gmail.com>
2019-11-21 19:51:56 -0800

    add more test arrays

--- Challenges/MergeSort/MergeSort/Program.cs
+++ Challenges/MergeSort/MergeSort/Program.cs
@@ -7,8 +7,12 @@ namespace MergeSort
         static void Main(string[] args)
         {
             int[] array = { 8,4,23,42,16,15 };
- 
-        }
+            MergeSort(array);
+
+            int[] array 2 = [20, 18, 12, 8, 5, -2]
+Few uniques: [5,12,7,5,5,7]
+        Nearly-sorted: [2,3,5,7,13,11]
+    }
 
         static void MergeSort(int[] arr)
         {
