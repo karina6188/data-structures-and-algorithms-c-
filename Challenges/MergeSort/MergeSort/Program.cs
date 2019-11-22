5c5811de1046c7ef9db88fddbba52db6bddf8445
Karina Chen <karina6188@gmail.com>
2019-11-21 19:51:10 -0800

    add test arrays

--- Challenges/MergeSort/MergeSort/Program.cs
+++ Challenges/MergeSort/MergeSort/Program.cs
@@ -6,7 +6,8 @@ namespace MergeSort
     {
         static void Main(string[] args)
         {
-            
+            int[] array = { 8,4,23,42,16,15 };
+ 
         }
 
         static void MergeSort(int[] arr)
