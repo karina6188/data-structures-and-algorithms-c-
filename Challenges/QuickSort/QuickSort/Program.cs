d89c69a86d41e3d0e22ffa17b3b2d6800741275f
Karina Chen <karina6188@gmail.com>
2019-11-24 21:47:45 -0800

    added summary comments

--- Challenges/QuickSort/QuickSort/Program.cs
+++ Challenges/QuickSort/QuickSort/Program.cs
@@ -4,6 +4,11 @@ namespace QuickSort
 {
     class Program
     {
+        /// <summary>
+        /// Call Quicksort method to sort the array and set the left and right points to be index 0 and the last index of the array.
+        /// Call PrintArray method to print out arrays.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             int[] array = new int[] { 8, 4, 23, 42, 16, 15 };
@@ -31,6 +36,14 @@ namespace QuickSort
             PrintArray(Quicksort(array4, 0, array4.Length - 1));
         }
 
+        /// <summary>
+        /// This method calls Partition method and sends in the left and right index positions.
+        /// Quicksort method recursively calls itself to partitions the sub arrays and sort left and right sub arrays.
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
         static int[] Quicksort(int[] arr, int left, int right)
         {
             if (left < right)
@@ -43,6 +56,18 @@ namespace QuickSort
             return arr;
         }
 
+        /// <summary>
+        /// This method sets the most right element to be the pivot and sets an index tracker called low to be at the previous index of left.
+        /// Use for loop to loop through elements from left to right.
+        /// If the element is smaller than the pivot, move the low tracker to the next index position then runs Swap method.
+        /// After all the elements have been checked and compared to the pivot, they should be swapped to the left or the right side of the array.
+        /// Then calls another Swap method to move the pivot to be at the correct position within the array.
+        /// Now the elements which are smaller than the pivot are on the left side of it and the elements which are larger than the pivot are on the right side of it.
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
         static int Partition(int[] arr, int left, int right)
         {
             int pivot = arr[right];
@@ -60,6 +85,16 @@ namespace QuickSort
             return low + 1;
         }
 
+        /// <summary>
+        /// Takes in an array and two index trackers i and low.
+        /// Store the current element at index position i in variable temp.
+        /// Assign element at index position low to be the element at index position i.
+        /// Assign element at index potition low to be temp.
+        /// This completes the swap of the two elements at index positions i and low.
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <param name="i"></param>
+        /// <param name="low"></param>
         static void Swap(int[] arr, int i, int low)
         {
             int temp;
@@ -68,6 +103,10 @@ namespace QuickSort
             arr[low] = temp;
         }
 
+        /// <summary>
+        /// Use a for loop to print out all the elements of an array.
+        /// </summary>
+        /// <param name="arr"></param>
         static void PrintArray(int[] arr)
         {
             Console.Write("[");
