36f2ff44da7059f0856d4e2a0b1b258f70c44fa6
Karina Chen <karina6188@gmail.com>
2019-11-25 23:12:51 -0800

    Created Node and Hashtable class

---
+++ Data-Structures/FindRepeatedWord/FindRepeatedWord/Hashtable.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindRepeatedWord
+{
+    class Hashtable
+    {
+        static void Add(string key, string value)
+        {
+            int index = Hash(key);
+        }
+
+        static string Get(string key)
+        {
+            string defaultText = "The key cannot be found";
+            return defaultText;
+        }
+
+        static bool Contains(string key)
+        {
+            return true;    
+        }
+
+        static int Hash(string key)
+        {
+            int index = 0;
+            return index;
+        }
+    }
+}
