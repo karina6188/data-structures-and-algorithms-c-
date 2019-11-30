695fce7108a9b367663140467babd9e05d0cfffc
Karina Chen <karina6188@gmail.com>
2019-11-30 04:11:04 -0800

    add node class and Add() method

--- Data-Structures/HashTables/HashTables/Hashtable.cs
+++ Data-Structures/HashTables/HashTables/Hashtable.cs
@@ -6,10 +6,61 @@ namespace HashTables
 {
     class Hashtable
     {
-        static void Add(string key, string value)
+        /// <summary>
+        /// Set how many buckets are there in the hashtable
+        /// </summary>
+        public int Buckets { get; set; }
+
+        /// <summary>
+        /// Create a Node array to store multitple key/value pairs in each bucket
+        /// </summary>
+        public Node[] HashNode { get; set; }
+
+        /// <summary>
+        /// Hashtable constructor to set up the quantity for buckets and set up the same amount of node arrays to match the bucket quantity
+        /// </summary>
+        /// <param name="buckets"></param>
+        public Hashtable(int buckets)
+        {
+            Buckets = buckets;
+            HashNode = new Node[buckets];
+        }
+
+        /// <summary>
+        /// Takes in a key and a value.
+        /// First, it calls Hash() method and sends over the key to get an index position inside the hashtable.
+        /// Check if the index position already has the key. If yes, print to console window that the key already exists. Otherwise, stores the key/value pair data to the corresponding bucket and into the next available node.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public void Add(string key, string value)
         {
             int index = Hash(key);
 
+            if (Contains(key))
+            {
+                Console.WriteLine($"Key \"{key}\" already exists in bucket No.{index}\n");
+            }
+            else
+            {
+                if (HashNode[index] == null)
+                {
+                    Node newNode = new Node(key, value);
+                    HashNode[index] = newNode;
+                    Console.WriteLine($"Add key/value \"{key}/{value}\" in bucket No.{index}");
+                }
+                else
+                {
+                    Node newNode = new Node(key, value);
+                    Node current = HashNode[index];
+                    while (current.Next != null)
+                    {
+                        current = current.Next;
+                    }
+                    current.Next = newNode;
+                    Console.WriteLine($"Add key/value \"{key}/{value}\" in bucket No.{index}");
+                }
+            }
         }
 
         static string Get(string key)
---
+++ Data-Structures/HashTables/HashTables/Node.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashTables
+{
+    class Node
+    {
+        /// <summary>
+        /// Key of the key/value pair
+        /// </summary>
+        public string Key { get; set; }
+
+        /// <summary>
+        /// Value of the key/value pair
+        /// </summary>
+        public string Value { get; set; }
+
+        /// <summary>
+        /// Node's reference to the next node
+        /// </summary>
+        public Node Next { get; set; }
+
+        /// <summary>
+        /// Node constructor to specify the data stored on the node and a reference to the next node
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public Node(string key, string value)
+        {
+            Key = key;
+            Value = value;
+            Next = null;
+        }
+    }
+}
