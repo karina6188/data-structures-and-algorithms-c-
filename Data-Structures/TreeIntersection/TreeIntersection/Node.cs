bd58bdd5f63ddfbb60b90b727e9dca62053199b5
Karina Chen <karina6188@gmail.com>
2019-12-02 18:30:34 -0800

    add Node class

--- Data-Structures/TreeIntersection/TreeIntersection/Node.cs
+++ Data-Structures/TreeIntersection/TreeIntersection/Node.cs
@@ -4,7 +4,33 @@ using System.Text;
 
 namespace TreeIntersection
 {
-    class Node
+    public class Node
     {
+        // <summary>
+        /// Key of the key/value pair
+        /// </summary>
+        public string Key { get; set; }
+
+        /// <summary>
+        /// Value of the key/value pair
+        /// </summary>
+        public string Value { get; set; }
+
+        /// <summary>
+        /// Node's reference to the next node
+        /// </summary>
+        public Node Next { get; set; }
+
+        /// <summary>
+        /// Node constructor to specify the data stored on the node and a reference to the next node
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public Node(string key, string value)
+        {
+            Key = key;
+            Value = value;
+            Next = null;
+        }
     }
 }
