01e3aab2b9224d62f1bc580e3ab3961206ac54a2
Karina Chen <karina6188@gmail.com>
2020-01-15 23:34:13 -0800

    edit node class

--- Data-Structures/TreeIntersection/TreeIntersection/Node.cs
+++ Data-Structures/TreeIntersection/TreeIntersection/Node.cs
@@ -6,12 +6,14 @@ namespace TreeIntersection
 {
     public class Node<T>
     {
+        // Property - Node's value of a generic type
         public T Value { get; set; }
 
         public Node<T> LeftChild { get; set; }
 
         public Node<T> RightChild { get; set; }
 
+        // Node class constructor
         public Node(T value)
         {
             Value = value;
