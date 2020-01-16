80303ea5300c7e6acf4ab1ca575ce1bd04183609
Karina Chen <karina6188@gmail.com>
2020-01-15 23:35:22 -0800

    edit binaryTree class

--- Data-Structures/TreeIntersection/TreeIntersection/BinaryTree.cs
+++ Data-Structures/TreeIntersection/TreeIntersection/BinaryTree.cs
@@ -6,6 +6,7 @@ namespace TreeIntersection
 {
     public class BinaryTree
     {
+        // Property of class BinaryTree which is a Node<string> type called Root. This is the entry point of the binary tree
         public Node<string> Root { get; set; }
 
         // Constructor to assign the tree's Root value
