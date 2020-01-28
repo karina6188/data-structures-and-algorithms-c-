9b8a4bc629e6b6915f0ca43f178e1e4522e4b5f4
Karina Chen <karina6188@gmail.com>
2020-01-27 23:37:40 -0800

    append method

--- Data-Structures/LinkedList-Extended/LinkedList-Extended/LinkedList-Extended.cs
+++ Data-Structures/LinkedList-Extended/LinkedList-Extended/LinkedList-Extended.cs
@@ -8,11 +8,22 @@ namespace LinkedList_Extended
     {
         public Node Head { get; set; }
 
+        // Constructor
         public LinkedList_Extend()
         {
             Head = null;
         }
 
+        /// <summary>
+        /// This method takes in an integer value that is to be appended to the linked list
+        /// Create a new node with the value
+        /// If the linked list is empty, assign the linked list's head to be the new node then return true to terminate the method
+        /// If the linked list is not empty, use a tracker to track the node on the linked list starting from Head
+        /// Traverse the linked list until the node's next value is null, which means it reaches to the end of the linked list
+        /// Then add the new node to the last node's Next of the linked list
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>A boolean that shows if a new node is appended to the linked list</returns>
         public bool Append(int value)
         {
             Node newNode = new Node(value);
