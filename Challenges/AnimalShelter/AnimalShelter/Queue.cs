68f596e6332b89b3e4829dc7cd7056fcf7a43da9
Karina Chen <karina6188@gmail.com>
2020-02-03 23:58:14 -0800

    add summary comments

--- Challenges/AnimalShelter/AnimalShelter/Queue.cs
+++ Challenges/AnimalShelter/AnimalShelter/Queue.cs
@@ -7,9 +7,18 @@ namespace AnimalShelter
 {
     public class Queue
     {
+        /// <summary>
+        /// Create two stacks to put animals
+        /// </summary>
         public Stack stack1 = new Stack();
         public Stack stack2 = new Stack();
 
+        /// <summary>
+        /// This method takes no parameter and returns a string to indicate how many animals are in the stack
+        /// First, push all the existing animals into stack 1 using the pre-determined Push method
+        /// For each animal in the stack, write to the console of each animal
+        /// </summary>
+        /// <returns></returns>
         public string existingAnimals()
         {
             stack1.Push("dog");
@@ -26,6 +35,15 @@ namespace AnimalShelter
             return allAnimals;
         }
 
+        /// <summary>
+        /// This method takes a parameter of a string data type and returns a string
+        /// If the animal from the parameter is either a dog or a cat, while stack 1 is not empty, keep popping the dogs and cats from stack 1 and push them to stack 2 until all the animals from stack 1 have been popped
+        /// Now stack 1 is empty. Push the animal from the parameter to stack 1
+        /// Then while stack 2 is not empty, keep popping all the animals from stack 2 to stack 1
+        /// Finally, write to the console of all the animals in the stack 1
+        /// </summary>
+        /// <param name="animal"></param>
+        /// <returns>A string to show cats and dogs in the stack. If an animal that is neither a cat nor a dog that is pushed to the stack, the string shows a message of "The shelter only takes dogs or cats"</returns>
         public string enqueue(string animal)
         {
             if (animal == "dog" || animal == "cat")
