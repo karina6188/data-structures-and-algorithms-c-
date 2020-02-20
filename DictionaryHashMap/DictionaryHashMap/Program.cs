0c63087cd7920ece46d1dd7c41eaa71e0fd00aa7
Karina Chen <karina6188@gmail.com>
2020-02-20 13:30:30 -0800

    use dictionary as a hashmap

---
+++ DictionaryHashMap/DictionaryHashMap/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryHashMap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 0, 3, -2, 5, 8 };
+            int target = 6;
+            TwoSum(nums, target);
+        }
+
+        static int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> dictionary = new Dictionary<int, int> { };
+            for (int i = 0; i < nums.Length; i++)
+            {
+                dictionary.Add(i, nums[i]);
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int complement = target - nums[i];
+                if (hashTable.ContainsKey[])
+            }
+
+        }
+    }
+}
